fca9fa4e2758b987f43563efc638ccd802d7418a
Sambasiva9 <to.nsamba@gmail.com>
2017-11-19 12:01:48 -0500

    hello

--- src/Sample.groovy
+++ src/Sample.groovy
@@ -1,5 +1,13 @@
 def loadSample(){
 echo " you are in groovy"
+
+def valueMap = [:]
+def envFile = readFile encoding: 'UTF-8', file: "src/meta/env.json"
+def envText = new groovy.json.JsonSlurper().parseText(envFile)
+
+valueMap.put("url", envText.PFDC_EPM_SERVICE_UTL)
+
+echo "Service url is :"+envText.PFDC_EPM_SERVICE_UTL
 }
 
 return this;
