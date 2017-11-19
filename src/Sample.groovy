782d5debc4cb50c0ffd9c4e7d211796ce62fb81e
Sambasiva9 <to.nsamba@gmail.com>
2017-11-19 17:35:42 -0500

    hello

--- src/Sample.groovy
+++ src/Sample.groovy
@@ -9,6 +9,21 @@ valueMap.put("url", envText.PFDC_EPM_SERVICE_UTL)
 
 echo "Service url is : Values inserted in map"+envText.PFDC_EPM_SERVICE_UTL
 
+configFileProvider(
+        [configFile(fileId: 'my-managed', variable: 'job_settings')]) {
+        props = readProperties file: 'src/constants.properties'
+        echo "${props}"
+    }
+
+	echo "Service url is after"
+	
+configFileProvider(
+        [configFile(fileId: 'my-managed-file', targetLocation: '/src/constants.properties')]) {
+        load '/path/to/job_settings_dir/my-managed-file'
+		}
+		
+		echo "Service url is after - 1"
+		
 def props = readProperties  file:'src/constants.properties'
 def Var1= props['pfdc_epam_service_rul']
 echo "Var1 in property file is=${Var1}"
