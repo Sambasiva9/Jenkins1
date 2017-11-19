815172d15251b5a1bd1474e5ae3ad56389f558fd
Sambasiva9 <to.nsamba@gmail.com>
2017-11-19 12:37:44 -0500

    hello

--- src/Sample.groovy
+++ src/Sample.groovy
@@ -7,7 +7,11 @@ def envText = new groovy.json.JsonSlurper().parseText(envFile)
 
 valueMap.put("url", envText.PFDC_EPM_SERVICE_UTL)
 
-echo "Service url is : Values inserted in map"
+echo "Service url is : Values inserted in map"+envText.PFDC_EPM_SERVICE_UTL
+
+def props = readProperties  file:'src/constants.properties'
+def Var1= props['pfdc_epam_service_rul']
+echo "Var1 in property file is=${Var1}"
 }
 
 return this;
