f81142ddfa200f4978c3efc50e418aa1b319e089
Sambasiva9 <to.nsamba@gmail.com>
2017-11-25 12:22:08 -0500

    sdfs

--- src/Sample.groovy
+++ src/Sample.groovy
@@ -9,6 +9,8 @@ valueMap.put("url", envText.PFDC_EPM_SERVICE_UTL)
 
 echo "Service url is : Values inserted in map"+envText.PFDC_EPM_SERVICE_UTL
 
+env.Java_home = tool ${envText.jdkVersion}
+
 
 	echo "Service url is after"
 	
